2cea62caa3b5e32bdfe7b8957102bbfdbd0af253
JominJun <mj3029327@naver.com>
2020-09-10 18:21:10 +0900

    코드 갈아엎기
    JSON으로 반환, 코드 beautify

--- main.py
+++ main.py
@@ -1,79 +1,31 @@
-import time
+import time, json
 
 day = int(time.strftime("%d", time.localtime(time.time())))
-std_presenter = list() # 날짜로 인해 선택된 자
-total_presenter = list()
-add_presenter = list() # 덧셈으로 인해 선택된 자
-add_add_presenter = list()
-add_mul_presenter = list()
-add_con_presenter = list()
-mul_presenter = list() # 곱셈으로 인해 선택된 자
-mul_add_presenter = list()
-mul_mul_presenter = list()
-mul_con_presenter = list()
-con_presenter = list() # 숫자 반전으로 인해 선택된 자
-con_add_presenter = list()
-con_mul_presenter = list()
-con_con_presenter = list()
+std_presenter = sorted([i for i in range(1, 36) if i%10 == day%10], key = lambda k:abs(k-day))
+plus_presenter = [i for i in range(1, 36) if i%10 == (int(str(std_presenter[-1])[0])+int(str(std_presenter[-1])[1]))%10]
+mul_presenter = [i for i in range(1, 36) if i%10 == (int(str(std_presenter[-1])[0])*int(str(std_presenter[-1])[1]))%10]
+reverse_presenter =[i for i in range(1, 36) if i%10 == (int(str(std_presenter[-1])[1]+str(std_presenter[-1])[0]))%10]
 
-def condition(inp):
-    return inp<=35
+result = {
+    "Origin": std_presenter,
+    "Tree 1": { # 덧셈
+        "Tree 1": plus_presenter,
+        "Tree 1-1": [i for i in range(1, 36) if i%10 == (int(str(plus_presenter[-1])[0])+int(str(plus_presenter[-1])[1]))%10], # 덧셈
+        "Tree 1-2": [i for i in range(1, 36) if i%10 == (int(str(plus_presenter[-1])[0])*int(str(plus_presenter[-1])[1]))%10], # 곱셈
+        "Tree 1-3": [i for i in range(1, 36) if i%10 == (int(str(plus_presenter[-1])[1]+str(plus_presenter[-1])[0]))%10]       # 전환
+    },
+    "Tree 2": { # 곱셈
+        "Tree 2": mul_presenter,
+        "Tree 2-1": [i for i in range(1, 36) if i%10 == (int(str(mul_presenter[-1])[0])+int(str(mul_presenter[-1])[1]))%10], # 덧셈
+        "Tree 2-2": [i for i in range(1, 36) if i%10 == (int(str(mul_presenter[-1])[0])*int(str(mul_presenter[-1])[1]))%10], # 곱셈
+        "Tree 2-3": [i for i in range(1, 36) if i%10 == (int(str(mul_presenter[-1])[1]+str(mul_presenter[-1])[0]))%10]       # 전환
+    },
+    "Tree 3": { # 전환
+        "Tree 3": reverse_presenter,
+        "Tree 3-1": [i for i in range(1, 36) if i%10 == (int(str(reverse_presenter[-1])[0])+int(str(reverse_presenter[-1])[1]))%10], # 덧셈
+        "Tree 3-2": [i for i in range(1, 36) if i%10 == (int(str(reverse_presenter[-1])[0])*int(str(reverse_presenter[-1])[1]))%10], # 곱셈
+        "Tree 3-3": [i for i in range(1, 36) if i%10 == (int(str(reverse_presenter[-1])[1]+str(reverse_presenter[-1])[0]))%10]       # 전환
+    }
+}
 
-def returnCMAX(inp):
-    return 3 if int(str(inp)[-1]) >= 6 else 4
-
-def fillList(arr, inp):
-    for i in range(returnCMAX(inp)):
-        k = 30 if inp + i * 10 >= 35 else 0
-        arr.append(inp + i * 10 - k)
-    return arr
-
-def maker(type, arr):
-    k = -1 if int(arr[-1]) >= 10 else -2
-    break_list = list(str(arr[k]))
-
-    if(type == "add"):
-        return int(break_list[0]) + int(break_list[1])
-    elif(type == "mul"):
-        return int(break_list[0]) * int(break_list[1])
-    elif(type == "con"):
-        if(break_list[1] == break_list[0]):
-            break_list = list(str(arr[k-1]))
-        return int(break_list[1]+break_list[0])
-
-print("날짜")
-print(fillList(std_presenter,day), end="\n\n")
-
-print("날짜->덧셈")
-print(fillList(add_presenter, maker("add", fillList(std_presenter, day))))
-print("날짜->덧셈->덧셈")
-print(fillList(add_add_presenter, maker("add", fillList(add_presenter, maker("add", fillList(std_presenter, day))))))
-print("날짜->덧셈->곱셈")
-print(fillList(add_mul_presenter, maker("mul", fillList(add_presenter, maker("add", fillList(std_presenter, day))))))
-print("날짜->덧셈->전환")
-print(fillList(add_con_presenter, maker("con", fillList(add_presenter, maker("add", fillList(std_presenter, day))))), end="\n\n")
-
-print("날짜->곱셈")
-print(fillList(mul_presenter, maker("mul", fillList(std_presenter, day))))
-print("날짜->곱셈->덧셈")
-print(fillList(mul_add_presenter, maker("add", fillList(add_presenter, maker("mul", fillList(std_presenter, day))))))
-print("날짜->곱셈-곱셈")
-print(fillList(mul_mul_presenter, maker("mul", fillList(add_presenter, maker("mul", fillList(std_presenter, day))))))
-print("날짜->곱셈->전환")
-print(fillList(mul_con_presenter, maker("con", fillList(add_presenter, maker("mul", fillList(std_presenter, day))))), end="\n\n")
-
-print("날짜->전환")
-print(fillList(con_presenter, maker("con", fillList(std_presenter, day))))
-print("날짜->전환->덧셈")
-print(fillList(con_add_presenter, maker("add", fillList(add_presenter, maker("con", fillList(std_presenter, day))))))
-print("날짜->전환->곱셈")
-print(fillList(con_mul_presenter, maker("mul", fillList(add_presenter, maker("con", fillList(std_presenter, day))))))
-print("날짜->전환->전환")
-print(fillList(con_con_presenter, maker("con", fillList(add_presenter, maker("con", fillList(std_presenter, day))))))
-
-total_presenter = std_presenter + add_presenter + add_add_presenter + add_mul_presenter + add_con_presenter + mul_presenter + mul_add_presenter + mul_mul_presenter + mul_con_presenter + con_presenter + con_add_presenter + con_mul_presenter + con_con_presenter
-total_presenter = list(set(total_presenter))
-total_presenter = list(filter(condition, total_presenter))
-
-print("=====[오늘 해야할 사람(최종)]=====")
-print(total_presenter)
+print(json.dumps(result, indent="\t"))
